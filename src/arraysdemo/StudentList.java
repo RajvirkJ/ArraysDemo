1976ec1df42fa6224a025bbcdb19335657db8128
Rajvir Kaur <kau15881@sheridancollege.ca>
2023-05-19 13:03:17 -0400

    first commit

---
+++ src/arraysdemo/StudentList.java
@@ -0,0 +1,18 @@
+
+package arraysdemo;
+
+
+public class StudentList {
+    public static void main(String[] args) {
+    Student s1 = new Student("s1", "Rajvir");
+    Student [] myArray = new Student [3];
+    
+    myArray[0]=s1;
+    myArray[1]=new Student("s2", "abc");
+    myArray[2]=new Student("s3", "xyz");
+    
+    for (int i=0; i<myArray.length;i++) {
+    System.out.println(myArray[i].getsName());
+    }
+    }
+}
